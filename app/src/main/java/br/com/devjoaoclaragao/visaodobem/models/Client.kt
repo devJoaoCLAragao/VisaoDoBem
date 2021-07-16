b6c3f4e960dcc27930779310bb30031290f6ee87
João Carlos Luzardo de Aragão <82767453+devJoaoCLAragao@users.noreply.github.com>
2021-07-16 19:29:56 -0300

    Implementção Adapter Client

---
+++ app/src/main/java/br/com/devjoaoclaragao/visaodobem/models/Client.kt
@@ -0,0 +1,9 @@
+package br.com.devjoaoclaragao.visaodobem.models
+
+class Client {
+    var name = String
+    var lastOrder = String
+    var photo = String
+    var clientSince = String
+    var recomendedBy = String
+}
---
+++ app/src/main/java/br/com/devjoaoclaragao/visaodobem/ui/adapters/ClientAdapter.kt
@@ -0,0 +1,39 @@
+package br.com.devjoaoclaragao.visaodobem.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.devjoaoclaragao.visaodobem.R
+import br.com.devjoaoclaragao.visaodobem.models.Client
+import com.bumptech.glide.Glide
+
+class ClientAdapter(var context : Context, var list : List<Client>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        var view = LayoutInflater.from(context).inflate(R.layout.item_list_client, parent, false)
+        return ClientHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ClientHolder).bind(context, list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
+
+class ClientHolder (view: View) : RecyclerView.ViewHolder(view){
+    fun bind(context: Context, client: Client) {
+        var photoClient = itemView.findViewById<ImageView>(R.id.photoClient)
+        var nameClient = itemView.findViewById<TextView>(R.id.nameClient)
+        var lastOrder = itemView.findViewById<TextView>(R.id.lastOrder)
+
+    }
+}
