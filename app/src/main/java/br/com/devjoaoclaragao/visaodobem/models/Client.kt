1dde4be1b89fd9063402121021bee339e52daaac
João Carlos Luzardo de Aragão <joaoclaragao@gmail.com>
2021-09-28 10:35:18 -0300

    #Develop
    Tela Cadastrar Cliente

--- app/src/main/java/br/com/devjoaoclaragao/visaodobem/models/Client.kt
+++ app/src/main/java/br/com/devjoaoclaragao/visaodobem/models/Client.kt
@@ -3,9 +3,19 @@ package br.com.devjoaoclaragao.visaodobem.models
 import java.io.Serializable
 
 data class Client (
-    var name : String = "",
+    var clientName : String = "",
+    var clientCPF : String = "",
+    var clientPhone : String = "",
+    var clientZip : String = "",
+    var clientStreet : String = "",
+    var clientNumber : String = "",
+    var clientComplement : String = "",
+    var clientDistrict : String = "",
+    var clientCity : String = "",
+    var clientRecomendedBy : String = "",
+    var clientBirthDate : String = "",
+
     var lastOrder : String = "",
     var photo : String = "",
-    var clientSince : String = "",
-    var recomendedBy : String = ""
+    var clientSince : String = ""
 ) : Serializable
